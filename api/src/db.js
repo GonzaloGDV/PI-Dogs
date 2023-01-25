2f69c435156779dc138a6a23d63123dcc6aff97d
Gonzalo Gomez <gonzalogdv@gmail.com>
2023-01-25 12:21:50 -0300

    Deploy Render

--- api/src/db.js
+++ api/src/db.js
@@ -4,40 +4,43 @@ const fs = require('fs');
 const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 
-const sequelize =
-  process.env.NODE_ENV === 'production'
-    ? new Sequelize(DB_DEPLOY, {
-        logging: false,
-        native: false,
-        pool: {
-          max: 3,
-          min: 1,
-          idle: 10000,
-        },
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-          KeepAlive: true,
-        },
-        ssl: true,
-      })
-    : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-        {
-          logging: false,
-          native: false,
-        }
-      );
+// const sequelize =
+//   process.env.NODE_ENV === 'production'
+//     ? new Sequelize(DB_DEPLOY, {
+//         logging: false,
+//         native: false,
+//         pool: {
+//           max: 3,
+//           min: 1,
+//           idle: 10000,
+//         },
+//         dialectOptions: {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//           },
+//           KeepAlive: true,
+//         },
+//         ssl: true,
+//       })
+//     : new Sequelize(
+//         `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//         {
+//           logging: false,
+//           native: false,
+//         }
+//       );
 
-// const sequelize = new Sequelize(
-//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-//   {
-//     logging: false, // set to console.log to see the raw SQL queries
-//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//   }
-// );
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
